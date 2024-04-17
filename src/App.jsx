89a103fa820d9d73482ab134f1c4caeb20375de8
ADITYARAJPUT2505 <ar9412349716@gmail.com>
2024-04-17 23:20:40 +0530

    programs added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import Navbar from'./components/navbar/navbar'
-import Hero from './components/hero/hero'
+import React from "react";
+import Navbar from "./components/navbar/navbar";
+import Hero from "./components/hero/hero";
+import Program from "./components/programs/programs";
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Program />
+      </div>
     </div>
   )
-}
+};
 
-export default App
+export default App;
