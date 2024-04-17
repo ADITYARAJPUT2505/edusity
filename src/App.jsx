31dd17697e17f6e943689c65bb904cc58a26b075
ADITYARAJPUT2505 <ar9412349716@gmail.com>
2024-04-17 23:34:21 +0530

    titles added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/navbar/navbar";
 import Hero from "./components/hero/hero";
 import Program from "./components/programs/programs";
+import Title from "./components/title/title"
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <div className="container">
+        <Title subTitle='Our Program' title='What We Offer' />
         <Program />
       </div>
     </div>
